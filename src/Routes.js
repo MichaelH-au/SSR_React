fc83ba04268471495c1124bb46377f73f9b6229a
Michael <huangjkau@gmail.com>
2019-01-02 22:16:30 +1100

    simple router

---
+++ src/Routes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from './containers/Home/Home'
+import Login from './containers/Login/Login'
+export default (
+    <div>
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/login' exact component={Login}></Route>
+    </div>
+)
